e734cfafcab750196eea252a4f1fd5bf2e9c15c8
smilysyndrella <baymax1298@gmail.com>
2017-04-19 14:18:53 +0530

    Update pro.js

--- pro.js
+++ pro.js
@@ -1,5 +1,6 @@
 var express = require("express"),
-    app     = express();
+    app     = express(),
+    port    = procss.env.PORT || 8000;
 
 app.use(express.static(__dirname + '/public'));
 
@@ -9,7 +10,7 @@ app.get("/", function(req, res){
     
 });
 
-app.listen(8000, function(){
+app.listen(port, function(){
     console.log("started");
     
 });
